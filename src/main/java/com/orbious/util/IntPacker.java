7b0002f40e541bad11d27fe9220a77d00f3328e0
davek <dklenowski@gmail.com>
2012-04-14 07:55:02 +1000

    tidy.

--- src/main/java/com/orbious/util/IntPacker.java
+++ src/main/java/com/orbious/util/IntPacker.java
@@ -7,23 +7,17 @@ public class IntPacker {
   private IntPacker() { }
 
   public static long pack(int i, int j) {
-    long l;
-
-    l = ((long)i << 32) | ((long)j & mask);
+    long l = ((long)i << 32) | ((long)j & mask);
     return l;
   }
 
   public static int i(long l) {
-    int i;
-
-    i = (int)(l >> 32);
+    int i = (int)(l >> 32);
     return i;
   }
 
   public static int j(long l) {
-    int i;
-
-    i = (int)(l & mask);
+    int i = (int)(l & mask);
     return i;
   }
 }
