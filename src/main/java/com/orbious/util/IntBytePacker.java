bc61ae1dd5c8ba02919e04e18fe026f49eb8546b
davek <dklenowski@gmail.com>
2012-09-18 22:48:56 +1000

    added intbytepacker. version bump.

---
+++ src/main/java/com/orbious/util/IntBytePacker.java
@@ -0,0 +1,94 @@
+package com.orbious.util;
+
+public class IntBytePacker {
+
+  public static int pack(byte i, byte j, byte k, byte l) {
+    return 
+        ((int)(i & 0xff) << 24) |
+        ((int)(j & 0xff) << 16) |
+        ((int)(k & 0xff) << 8) |
+        ((int)(l & 0xff));
+  }
+  
+  public static int set(int i, int idx, byte b) {
+    int shift;
+    switch ( idx ) {
+    case 0:
+      shift = 24;
+      break;
+    case 1:
+      shift = 16;
+      break;
+    case 2:
+      shift = 8;
+      break;
+    default:
+      shift = 0;
+      break;
+    }
+    
+    return ((int)b & 0xff) << shift | i;
+  }
+  
+  public static int setnext(int i, byte b) {
+    int shift;
+    
+    if ( (byte)(0xff & (i >> 24)) == 0 ) {
+      shift = 24;
+    } else if ( (byte)(0xff & (i >> 16)) == 0 ) {
+      shift = 16;
+    } else if ( (byte)(0xff & (i >> 8)) == 0 ) {
+      shift = 8;
+    } else if ( (byte)(0xff & i) == 0 ) {
+      shift = 0;
+    } else {
+      throw new ArrayIndexOutOfBoundsException("No space left in " + i);
+    }
+    
+    return ((int)b & 0xff) << shift | i;
+  }
+  
+  public static byte[] unpack(int i) {
+    byte[] b = new byte[4];
+
+    b[0] = (byte)(0xff & (i >> 24));
+    b[1] = (byte)(0xff & (i >> 16));
+    b[2] = (byte)(0xff & (i >> 8));
+    b[3] = (byte)(0xff & i);
+
+    return b;
+  }
+  
+  public static int[] unpack2(int i) {
+    int[] a = new int[4];
+
+    a[0] = (int)(0xff & (i >> 24));
+    a[1] = (int)(0xff & (i >> 16));
+    a[2] = (int)(0xff & (i >> 8));
+    a[3] = (int)(0xff & i);
+
+    return a;
+  }
+  
+  public static byte get(int i, int idx) {
+    int shift;
+    switch ( idx ) {
+    case 0:
+      shift = 24;
+      break;
+    case 1:
+      shift = 16;
+      break;
+    case 2:
+      shift = 8;
+      break;
+    default:
+      shift = 0;
+      break;
+    }
+    
+    return (byte)(0xff & (i >> shift));
+  }
+  
+  
+}
---
+++ src/test/java/com/orbious/util/IntBytePackerTest.java
@@ -0,0 +1,92 @@
+package com.orbious.util;
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+public class IntBytePackerTest {
+
+  @Test
+  public void pack_unpack() throws Exception {
+    int in;
+    byte[] b;
+    int[] is;
+    
+    in = IntBytePacker.pack((byte)1, (byte)2, (byte)3, (byte)4);
+    b =  IntBytePacker.unpack(in);
+    for ( int i = 0; i < b.length; i++ ) 
+      Assert.assertEquals((i+1), (int)b[i]);
+
+    is = IntBytePacker.unpack2(in);
+    for ( int i = 0; i < b.length; i++ ) 
+      Assert.assertEquals((i+1), is[i]);
+    
+    in = IntBytePacker.pack(Byte.MAX_VALUE, Byte.MAX_VALUE, Byte.MAX_VALUE, Byte.MAX_VALUE);
+    b = IntBytePacker.unpack(in);
+    for ( int i = 0; i < b.length; i++ ) 
+      Assert.assertEquals(Byte.MAX_VALUE, b[i]);
+
+    is = IntBytePacker.unpack2(in);
+    for ( int i = 0; i < b.length; i++ ) 
+      Assert.assertEquals((int)Byte.MAX_VALUE, is[i]);
+    
+    in = IntBytePacker.pack(Byte.MIN_VALUE, Byte.MIN_VALUE, Byte.MIN_VALUE, Byte.MIN_VALUE);
+    b = IntBytePacker.unpack(in);
+    for ( int i = 0; i < b.length; i++ ) 
+      Assert.assertEquals(Byte.MIN_VALUE, b[i]);
+    
+    is = IntBytePacker.unpack2(in);
+    for ( int i = 0; i < b.length; i++ ) 
+      Assert.assertEquals((int)Byte.MIN_VALUE, is[i]);
+  }
+
+  @Test
+  public void getset() throws Exception {
+    int in;
+    byte[] b;
+    
+    in =0;
+    for ( int i = 0; i < 4; i++ ) 
+      in = IntBytePacker.set(in, i, (byte)(i+1));
+    
+    b =  IntBytePacker.unpack(in);
+    for ( int i = 0; i < b.length; i++ ) {
+      Assert.assertEquals((i+1), (int)b[i]);
+      Assert.assertEquals((i+1), IntBytePacker.get(in, i));
+    }
+    
+    in = 0;
+    for ( int i = 0; i < 4; i++ ) {
+      in = IntBytePacker.set(in, i, Byte.MAX_VALUE);
+      Assert.assertEquals(Byte.MAX_VALUE, IntBytePacker.get(in, i));
+    }
+    
+    b =  IntBytePacker.unpack(in);
+    for ( int i = 0; i < b.length; i++ ) 
+      Assert.assertEquals(Byte.MAX_VALUE, (int)b[i]);
+    
+    in = 0;
+    for ( int i = 0; i < 4; i++ ) 
+      in = IntBytePacker.set(in, i, Byte.MIN_VALUE);
+    
+    b =  IntBytePacker.unpack(in);
+    for ( int i = 0; i < b.length; i++ ) 
+      Assert.assertEquals(Byte.MIN_VALUE, (int)b[i]);
+  }
+ 
+  @Test
+  public void setnext() throws Exception {
+    int in;
+    byte[] b;
+
+    in = 0;
+    in = IntBytePacker.setnext(in, (byte)1);
+    in = IntBytePacker.setnext(in, (byte)2);
+    in = IntBytePacker.setnext(in, (byte)3);
+    in = IntBytePacker.setnext(in, (byte)4);
+    
+    b =  IntBytePacker.unpack(in);
+    for ( int i = 0; i < b.length; i++ ) 
+      Assert.assertEquals((i+1), (int)b[i]);
+  }
+  
+}
