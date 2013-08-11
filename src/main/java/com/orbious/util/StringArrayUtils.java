242255f957a5bb00dddc290d2e0bc728ba74b810
davek <dklenowski@gmail.com>
2013-08-11 18:56:36 +1000

    added some string utils. version bump.

---
+++ src/main/java/com/orbious/util/StringArrayUtils.java
@@ -0,0 +1,25 @@
+package com.orbious.util;
+
+import java.util.ArrayList;
+
+public class StringArrayUtils {
+  
+  private StringArrayUtils() { }
+  
+  public static String[] unpad(final String[] a) {
+    ArrayList<String> al = new ArrayList<String>();
+    for ( int i = 0; i < a.length; i++ ) {
+      if ( a[i] != null ) al.add(a[i]);
+    }
+
+    return al.toArray(new String[al.size()]);
+  }
+  
+  public static boolean allpad(final String[] a) {
+    for ( int i = 0; i < a.length; i++ ) 
+      if ( a[i] != null ) return false;
+    
+    return true;
+  }
+
+}
